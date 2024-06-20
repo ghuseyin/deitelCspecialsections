4448d8f7bb26f31926f088f01ddbaa572f449dbc
ghuseyin <guzelhg1@gmail.com>
2024-06-20 22:19:35 +0300

    Bugfix and 7.30.i
    * Added GETSTR operation. 7.30.i
    * Added POW with negative numbers.
    * Fixed false function parameters in READ operation.
    * Added error handling messages for "dump" and "read_hexf" function calls.
    * Global arrays moved into related functions. Data sharing between functions provided by pointers.

--- Simpletron Simulator V2(Exercise 7.30).c
+++ Simpletron Simulator V2(Exercise 7.30).c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 //Defining SML Operation Codes
 //I/O
@@ -12,15 +13,16 @@
 #define SUBTRACT 0x31
 #define DIVIDE 0x32
 #define MULTIPLY 0x33
-#define MOD 0x34  // 7.30.b
-#define POW 0x35  // 7.30.c
+#define MOD 0x34        // 7.30.b
+#define POW 0x35        // 7.30.c
 //Transfer of Control
 #define BRANCH 0x40
 #define BRANCHNEG 0x41
 #define BRANCHZERO 0x42
 #define HALT 0x43
 //Character Operations
-#define NEWLINE 0x50  // 7.30.e
+#define NEWLINE 0x50    // 7.30.e
+#define GETSTR 0x51     // 7.30.i
 
 //Defining Computer Specifications
 #define W_MAX 0xFFFF    // Max word size is 4 hex digit: FFFF
@@ -29,8 +31,8 @@
 // 7.30.a says 1000 (0x400) but more than FF not usable for a 4-digit instruction computer.
 
 char* lf2hex(double lfvalue);       // For printing floats as hex 
-int read_hexf(const size_t index, double *writeAdrr);   // Read float as hex
-char HEXF[14] = {0}, TEMP[11]= {0};
+int read_hexf(const size_t index, double *writeAdrr, int readInstBit);   // Read float as hex
+int get_string(double* writeAdrr);
 
 int main(void){
     void welcome(void);
@@ -49,7 +51,8 @@ int main(void){
     //Reading instructions
     if(read_inst(&instCounter, memory)){
         puts("Error: Program loading error!!");
-        dump(instCounter, operand, opCode, instRegister, accumulator, memory);
+        if(dump(instCounter, operand, opCode, instRegister, accumulator, memory))
+            puts("Error: Computer dump error!!");
         return -1;
     }
     else
@@ -59,7 +62,8 @@ int main(void){
     //Executing instructions
     if(execute(&instCounter, &operand, &opCode, &instRegister, &accumulator, memory)){
         puts("Error: Program execution abnormally terminated!!");
-        dump(instCounter, operand, opCode, instRegister, accumulator, memory);
+        if(dump(instCounter, operand, opCode, instRegister, accumulator, memory))
+            puts("Error: Computer dump error!!");
         return -2;
     }
     else
@@ -71,7 +75,6 @@ int main(void){
         puts("Error: Computer dump error!!");
         return -3;
     }
-
     return 0;
 }
 
@@ -92,7 +95,10 @@ int read_inst(size_t* const pInstCounter, double memory[MEMSIZE]){
         "***        Program loading started              ***");
     // Instruction reading cycle
     for(*pInstCounter = 0; *pInstCounter < MEMSIZE; (*pInstCounter)++){
-        read_hexf(*pInstCounter, &memory[*pInstCounter]);
+        if(read_hexf(*pInstCounter, &memory[*pInstCounter], 1)){
+            puts("Error: Instructions can not be read!!");
+            return 5;
+        }
         if(0xFFFFF==(int)memory[*pInstCounter]) break;
     }
     return 0;
@@ -107,7 +113,7 @@ int execute(size_t* const pInstCounter, size_t* const pOperand, int* const pOpCo
     // Instruction execution cycle
     for (*pInstCounter = 0; *pInstCounter < MEMSIZE; (*pInstCounter)++){
 
-        *pInstRegister = (int)memory[*pInstCounter]; // Fetch data from memory
+        *pInstRegister = (int)memory[*pInstCounter];    // Fetch data from memory
         if(*pInstRegister<0x1000 || *pInstRegister>0x6000){ 
             printf("%04X There is no any operation code. Program will continue.\n",
                 *pInstRegister);
@@ -120,7 +126,10 @@ int execute(size_t* const pInstCounter, size_t* const pOperand, int* const pOpCo
         // Selecting and executing instruction
         switch (*pOpCode){
         case READ:
-            read_hexf(*pOperand, &memory[*pOperand]);
+            if(read_hexf(*pOperand, &memory[*pOperand], 0)){
+                puts("Error: Data can not be read!!");
+                return 5;
+            }
             break;
 
         case WRITE:
@@ -199,16 +208,20 @@ int execute(size_t* const pInstCounter, size_t* const pOperand, int* const pOpCo
         
         case POW:
             if(memory[*pOperand]>0)
-                for(int i = 1, temp=*pAccumulator; i < memory[*pOperand]; i++){
-                    *pAccumulator *= temp;
-                }
-            else if(0==(int)memory[*pOperand]){
+                for(int i = 1, base=*pAccumulator; i < memory[*pOperand]; i++)
+                    *pAccumulator *= base;
+            
+            else if(0==(int)memory[*pOperand])
                 *pAccumulator = 1;
-            }
-            else if(memory[*pOperand]<0){     //     TODO after 7.30.f
-                printf("Power with negative numbers not supported yet.\n"
-                    "Program canceling.\n");
-                return 4;
+            
+            else if(memory[*pOperand]<0){
+                if(0==(int)*pAccumulator){
+                    printf("%04X Error: Attempt to divide by zero!!\n", *pInstRegister);
+                    return 2;
+                }
+                *pAccumulator = 1 / *pAccumulator;
+                for(int i = 1, base=*pAccumulator; i < memory[*pOperand]; i++)
+                    *pAccumulator *= base;
             }
             printf("Accumulator ^ %s is %s.\n",
                 lf2hex(memory[*pOperand]), lf2hex(*pAccumulator));
@@ -240,29 +253,33 @@ int execute(size_t* const pInstCounter, size_t* const pOperand, int* const pOpCo
             printf("%04X Printed new line.\n\n", *pInstRegister);
             break;
 
+        case GETSTR:
+            printf("%04X Reading string: ", *pInstRegister);
+            get_string(&memory[*pOperand]);
+            break;
+
         default:
             printf("%04X There is no any operation code. Program will continue.\n",
                 *pInstRegister);
             break;
         }
     }
-    return 1; // Program ended without "Halt" operation.
+    return 1;   // Program ended without "Halt" operation.
 }
 
 int dump(const size_t instCounter, const size_t operand, const int opCode,
     const int instRegister, const double accumulator,const double memory[MEMSIZE]){
     
     puts("*** Computer Dump Printing ***");
-    printf("\nREGISTERS(hex):\n"
+    printf("\nREGISTERS(hex):\n"    // Printing registers
         "accumulator:\t\t%s\n"
         "instructionCounter:\t     %02lX\n"
         "instructionRegister:\t   %04X\n"
         "operationCode:\t\t     %02X\n"
-        "operand:\t\t     %02lX\n"
-        "HEXF:\t%s\nTEMP:\t%s\n\n",
-        lf2hex(accumulator), instCounter, instRegister, opCode, operand, HEXF, TEMP);
+        "operand:\t\t     %02lX\n\n",
+        lf2hex(accumulator), instCounter, instRegister, opCode, operand);
 
-    puts("MEMORY(hex):");
+    puts("MEMORY(hex):");           // Printing memory dump
     printf("%s", "  ");
     for (int i = 0; i < 0x10; i++)
         printf("  %7X", i);
@@ -279,21 +296,40 @@ int dump(const size_t instCounter, const size_t operand, const int opCode,
 }
 
 char* lf2hex(double lfvalue){
-    sprintf(HEXF, "%04X.%02x", (int)lfvalue, (int)(lfvalue*0x100)%0x100);
-    return HEXF;
+    static char hexf[15];
+    sprintf(hexf, "%04X.%02x", (int)lfvalue, (int)(lfvalue*0x100)%0x100);
+    return hexf;
 }
 
-int read_hexf(const size_t index, double *writeAdrr){
+int read_hexf(const size_t index, double *writeAdrr, int readInstBit){
+    char input[7]= {0}, output[10]= {0};
     do{
         printf("%02lX ? ", index);
-        scanf("%10s", TEMP);    // Max input 10 character
-        if('-'==TEMP[0]){
-            TEMP[0] = '0';
-            sprintf(HEXF, "-0x%s", TEMP);
+        fgets(input, 7, stdin);     // Max input 6 character
+        if('-'==input[0]){
+            input[0] = '0';
+            sprintf(output, "-0x%s", input);
         }
         else
-            sprintf(HEXF, "0x%s", TEMP);
-        sscanf(HEXF, "%lf", writeAdrr);
-    }while((*writeAdrr < W_MIN || *writeAdrr > W_MAX) && 0xFFFFF!=(int)(*writeAdrr));
+            sprintf(output, "0x%s", input);
+        sscanf(output, "%lf", writeAdrr);
+    }while((*writeAdrr < W_MIN || *writeAdrr > W_MAX) && 
+        (0xFFFFF!=(int)(*writeAdrr) || 0==readInstBit) );
+    return 0;
+}
+
+int get_string(double* writeAdrr){
+    int strSize = ((int)(*writeAdrr) / 0x100)+1;
+    char buffer[strSize];
+    fgets(buffer, strSize, stdin);
+    for(size_t i=0; buffer[i]!='\0'; i++){
+        if(0==i%2){  // for writing to last 2 digit of memory word
+            *writeAdrr += buffer[i];
+            writeAdrr++;
+        }
+        else        // for writing to first 2 digit of memory word
+            *writeAdrr = buffer[i]*0x100;
+    }
+    printf("String saved.\n");
     return 0;
 }
